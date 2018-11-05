dd3035fbaffdc0fdd5512cef75f930c5db06ded5
billy <wschap@ufl.edu>
2018-11-05 14:30:02 -0500

    converting in skycoords

--- tablebuild.py
+++ tablebuild.py
@@ -7,7 +7,7 @@ from astropy.coordinates import SkyCoord
 
 
 starlist= open('binarylist.txt','r')
-df= pd.read_fwf('binarylist.txt', names= ['Binary'])
+df= pd.read_fwf('binarylist.txt', names= ['BinaryName'])
 starid=[]
 starra=[]
 stardec=[]
@@ -17,12 +17,14 @@ for binary in starlist:
     #idtable= Simbad.query_objectids(binary)
     paramtable= Simbad.query_object(binary)
     pdparam= paramtable.to_pandas() #convert to Pandas table
-    print(pdparam)
     pdparamfix= SkyCoord(pdparam['RA'], pdparam['DEC'], frame='icrs', unit='deg')
-    print(pdparamfix)
-    starra.append(pdparam['RA'][0])
+    radeg = (pdparamfix.ra*u.deg)
+    decdeg = (pdparamfix.dec*u.deg)
+    starra.append(radeg[0])
+    stardec.append(decdeg[0])
+    #starra.append(pdparam['RA'][0])
+    #stardec.append(pdparam['DEC'][0])
     
-    stardec.append(pdparam['DEC'][0])
     #idsearch=[x for x in idtable['ID'] if 'Gaia DR2' in x]
     #if len(idsearch) == 0:
     #    print(f"Gaia ID not found for {binary}")
@@ -36,6 +38,7 @@ for binary in starlist:
 df['RA']   = pd.Series(starra) 
 df['DEC']  = pd.Series(stardec) 
 
+df= df.replace("deg2","")
 print(df)
 df.to_csv('binarytable.txt')
 
